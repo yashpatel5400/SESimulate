7db7f01d3f1db0d100aac90efd6648c9215380ec
yashpatel5400 <ypatel@princeton.edu>
2015-07-30 14:46:37 -0400

    Updated network analysis to be Python 2 (for the purposes of freezing). Froze file for execution

--- NetworkAnalysis.py
+++ NetworkAnalysis.py
@@ -5,28 +5,36 @@
 # obtains network metrics (i.e. degree distribution, density, etc..)#
 #####################################################################
 
+from __future__ import division
+from __future__ import absolute_import
 import networkx as nx
 import matplotlib.pyplot as plt
+import Tkinter
+import FileDialog
+
 from numpy import mean
+from io import open
+from itertools import imap
+from itertools import izip
 
 #####################################################################
 # Given the filename (MUST be .clu format), determines the degree   #
 # distribution and outputs a graphical representation of it         #
 #####################################################################
 def getDegreeDistribution(filename):
-	f = open(filename, 'r')
+	f = open(filename, u'r')
 	f.readline()
 	degrees = [int(line) for line in f]
 
 	degree_sequence=sorted(degrees,reverse=True)
 	dmax=max(degree_sequence)
 
-	plt.loglog(degree_sequence,'b-',marker='o')
-	plt.title("Degree rank plot")
-	plt.ylabel("degree")
-	plt.xlabel("rank")
+	plt.loglog(degree_sequence,u'b-',marker=u'o')
+	plt.title(u"Degree rank plot")
+	plt.ylabel(u"degree")
+	plt.xlabel(u"rank")
 
-	plt.savefig("{}Degree.png".format(filename[0:-4]))
+	plt.savefig(u"{}Degree.png".format(filename[0:-4]))
 	plt.show()
 
 #####################################################################
@@ -35,9 +43,9 @@ def getDegreeDistribution(filename):
 #####################################################################
 def getDensity(filename):
 	ID_LENGTH = 10
-	f = open(filename, 'r')
+	f = open(filename, u'r')
 	numVertices = int(f.readline()[ID_LENGTH:])
-	for i in range(numVertices): 
+	for i in xrange(numVertices): 
 		f.readline()
 	f.readline()
 	numEdges = len([line for line in f])
@@ -49,10 +57,10 @@ def getDensity(filename):
 def getReachability(G):
 	numNodes = len(G.nodes())
 	# Obtains all the reability values for the nodes in the graph
-	reaches = list(map(lambda node: len(nx.descendants(G, 
+	reaches = list(imap(lambda node: len(nx.descendants(G, 
 		node))/numNodes, G.nodes()))
 	avgReach = mean(reaches)
-	indReache = dict(zip(G.nodes(), reaches))
+	indReache = dict(izip(G.nodes(), reaches))
 	return avgReach
 
 #####################################################################
@@ -64,10 +72,10 @@ def getEccentricity(G):
 	# Obtains all the eccentricities for nodes in the graph. Note: the
 	# -1 in the calculation accounts for not including the starting 
 	# node when calculating the distance of paths
-	ecc = [max(list(map(lambda x: len(paths[node][x]) - 1, paths[node]))) \
+	ecc = [max(list(imap(lambda x: len(paths[node][x]) - 1, paths[node]))) \
 		for node in paths]
-	eccentricities = dict(zip(paths, ecc))
-	avgEccentricity = sum(map(lambda x: eccentricities[x], 
+	eccentricities = dict(izip(paths, ecc))
+	avgEccentricity = sum(imap(lambda x: eccentricities[x], 
 		eccentricities))/len(eccentricities)
 	return avgEccentricity
 
@@ -79,23 +87,19 @@ def getGeodesic(G):
 	geoDist = nx.average_shortest_path_length(G1)
 	return geoDist
 
-if __name__ == "__main__":
-	resultType = input("Please enter (c) for closeness and "\
-		"(t) for talking: ")
+if __name__ == u"__main__":
+	ADJUST = 1
+	months = int(raw_input(u"Please number of months: "))
+	for month in range(months):
+		for typeStr in [u"Close", u"Talk"]:
+			degreeDist = u"{}Month_{}.clu".format(typeStr, month + ADJUST)
+			network = u"{}Results_Month{}.net".format(typeStr, month + ADJUST)
 
-	isCloseness = (resultType == 'c')
-	if isCloseness: typeStr = "Close"
-	else: typeStr = "Talk"
+			G = nx.read_pajek(network)
+			G1 = nx.Graph(G)
 
-	month = input("Please enter in month: ")
-	degreeDist = "{}Month_{}.clu".format(typeStr, month)
-	network = "{}Results_Month{}.net".format(typeStr, month)
-
-	G = nx.read_pajek(network)
-	G1 = nx.Graph(G)
-
-	getDegreeDistribution(degreeDist)
-	print("Degree density: {}".format(getDensity(network)))
-	print("Eccentricity: {}".format(getEccentricity(G1)))
-	print("Geodesic Distance: {}".format(getGeodesic(G1)))
-	print("Reachability: {}".format(getReachability(G1)))
+			getDegreeDistribution(degreeDist)
+			print u"Degree density: {}".format(getDensity(network))
+			print u"Eccentricity: {}".format(getEccentricity(G1))
+			print u"Geodesic Distance: {}".format(getGeodesic(G1))
+			print u"Reachability: {}".format(getReachability(G1))
