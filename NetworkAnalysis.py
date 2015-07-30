9a0b9932515ee41946313fa9ccf834c7b6f96e54
yashpatel5400 <ypatel@princeton.edu>
2015-07-30 13:09:32 -0400

    Added comments to network analysis file

--- NetworkAnalysis.py
+++ NetworkAnalysis.py
@@ -1,7 +1,18 @@
+#####################################################################
+# Name: Yash Patel                                                  #
+# File: NetworkAnalysis.py                                          #
+# Description: Given data (formatted as Pajek .net and .clu files)  #
+# obtains network metrics (i.e. degree distribution, density, etc..)#
+#####################################################################
+
 import networkx as nx
 import matplotlib.pyplot as plt
 from numpy import mean
 
+#####################################################################
+# Given the filename (MUST be .clu format), determines the degree   #
+# distribution and outputs a graphical representation of it         #
+#####################################################################
 def getDegreeDistribution(filename):
 	f = open(filename, 'r')
 	f.readline()
@@ -18,6 +29,10 @@ def getDegreeDistribution(filename):
 	plt.savefig("{}Degree.png".format(filename[0:-4]))
 	plt.show()
 
+#####################################################################
+# Given the filename (MUST be .net format), determines the density  #
+# of the graph and returns it                                       #
+#####################################################################
 def getDensity(filename):
 	ID_LENGTH = 10
 	f = open(filename, 'r')
@@ -28,16 +43,27 @@ def getDensity(filename):
 	numEdges = len([line for line in f])
 	return numEdges/(numVertices * (numVertices - 1))
 
+#####################################################################
+# Given a graph G, determines the average reachability of its nodes #
+#####################################################################
 def getReachability(G):
 	numNodes = len(G.nodes())
+	# Obtains all the reability values for the nodes in the graph
 	reaches = list(map(lambda node: len(nx.descendants(G, 
 		node))/numNodes, G.nodes()))
 	avgReach = mean(reaches)
 	indReache = dict(zip(G.nodes(), reaches))
 	return avgReach
 
+#####################################################################
+# Given a graph G, determines the average eccentricity of its nodes #
+#####################################################################
 def getEccentricity(G):
 	paths = nx.shortest_path(G)
+
+	# Obtains all the eccentricities for nodes in the graph. Note: the
+	# -1 in the calculation accounts for not including the starting 
+	# node when calculating the distance of paths
 	ecc = [max(list(map(lambda x: len(paths[node][x]) - 1, paths[node]))) \
 		for node in paths]
 	eccentricities = dict(zip(paths, ecc))
@@ -45,6 +71,10 @@ def getEccentricity(G):
 		eccentricities))/len(eccentricities)
 	return avgEccentricity
 
+#####################################################################
+# Given a graph G, determines the average geodesic distance of its  #
+# nodes                                                             #
+#####################################################################
 def getGeodesic(G):
 	geoDist = nx.average_shortest_path_length(G1)
 	return geoDist
